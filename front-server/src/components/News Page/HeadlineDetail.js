8845b526cabcb3f715873f7844dc7905c1ec326b
fsanal <fsanal@sas.upenn.edu>
2020-04-24 19:56:31 -0700

    headline detail design almost complete

--- front-server/src/components/News Page/HeadlineDetail.js
+++ front-server/src/components/News Page/HeadlineDetail.js
@@ -6,7 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //components
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Button from 'react-bootstrap/Button'
-
+import Card from 'react-bootstrap/Card'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLaughBeam } from '@fortawesome/free-solid-svg-icons'
+import { TagCloud } from 'react-tagcloud'
+ 
 //styles
 import styled from "styled-components"
 
@@ -15,21 +19,68 @@ const HeadlineDetail = () => {
     return (
         <>
             <StyledJumbotron>
-                <Title>A 117-year-old woman in Mexico City finally received her birth certificate, 
+                <StyledIcon icon={faLaughBeam} />
+                <Wrapper>
+                    <h1>A 117-year-old woman in Mexico City finally received her birth certificate, 
                         and died a few hours later. Trinidad Alvarez Lira had waited years for proof that 
-                        she had been born in 1898.</Title>
+                        she had been born in 1898. </h1>
+                </Wrapper>
                 <Date>2016-07-01</Date>
-                <TopicWrapper>
+                <Wrapper>
                     <Topic background_color = '#9c88ff'>Immigration</Topic>
                     <Topic background_color = '#ff6348'>Death</Topic>
-                </TopicWrapper>
-                
+                </Wrapper>
+                <CardWrapper>
+                    <StyledCard >
+                        <Card.Body>
+                            <Sentiment>Sentiment: 0.85</Sentiment>
+                            <StyledText>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </StyledText>
+                            
+                        </Card.Body>
+                    </StyledCard>
+                    <StyledCard >
+                        <Card.Body>
+                            <Card.Title>Sentiment: 0.35</Card.Title>
+                            <StyledText>
+                            A woman in Mexico City finally received her birth certificate,
+                            </StyledText>
+                            
+                        </Card.Body>
+                    </StyledCard>
+                    <StyledCard >
+                        <Card.Body>
+                            <Card.Title>Sentiment: 0.25</Card.Title>
+                            
+                            <StyledText>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </StyledText>
+                            
+                        </Card.Body>
+                    </StyledCard>
+                    <StyledCard >
+                        <Card.Body>
+                            <Card.Title>Sentiment: 0.75</Card.Title>
+                            
+                            <StyledText>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </StyledText>
+                            
+                        </Card.Body>
+                    </StyledCard>
+                </CardWrapper>
             </StyledJumbotron>
         </>
     )
 }
 
 export default HeadlineDetail
+
+
 
 
 const StyledJumbotron = styled(Jumbotron)`
@@ -39,17 +90,45 @@ const StyledJumbotron = styled(Jumbotron)`
     margin-top: 20px;
     background-color: white !important;
 `
-const Title = styled.h1`
-    
+
+const StyledCard = styled(Card)`
+    display: inline-block !important;
+    margin-right: 36px;
+    width: 15rem !important;
+    height: 17rem !important;
 `
 
-const Date = styled.h4`
-    margin-top: 40px;
-    margin-left: 480px;
+const StyledText = styled(Card.Text)`
+    height: 5rem !important;
 `
 
-const TopicWrapper = styled.div`
+const StyledIcon = styled(FontAwesomeIcon)`
+    color: #2ecc71;
+    font-size: 100px;
+    margin-left: 45%;
+    margin-right:
+`
+
+const Date = styled.h3`
     margin-top: 30px;
+    margin-left: 43%;
+`
+
+const Wrapper = styled.div`
+    margin-top: 30px;
+    margin-left: 30px;
+    margin-right: auto;
+`
+
+const CardWrapper = styled.div`
+    margin-top: 30px;
+    margin-left: 30px;
+    margin-right: auto;
+    display:flex;
+`
+
+const Sentiment = styled(Card.Title)`
+    color: #2ecc71 !important;
 `
 
 
