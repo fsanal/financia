8ef2bf051e09f1d3b59c04d3cfa70eb3f5e251cf
mkongsiri <m_kongsiri@hotmail.com>
2020-04-25 12:29:50 -0400

    fixed key error

--- front-server/src/components/News Page/Headlines.js
+++ front-server/src/components/News Page/Headlines.js
@@ -50,17 +50,17 @@ class Headlines extends React.Component {
         var icons = [economy_icon, micro_icon, poli_icon, train_icon, travel_icon, video_icon, world_icon, poli_icon]
         console.log(this.props.headlines.length)
         if (this.props.headlines.length === 2) {
-            cards.push(<CardWrapper>
+            cards.push(<CardWrapper key={this.props.headlines[0].id}>
                 <HeadlineCard icon = {icons[this.rand(icons.length)]} key = {this.props.headlines[0].id} item = {this.props.headlines[0]}/>
                 <HeadlineCard icon = {icons[this.rand(icons.length)]} key = {this.props.headlines[1].id} item = {this.props.headlines[1]}/>
             </CardWrapper>);
         } else if (this.props.headlines.length === 1) {
-            cards.push(<CardWrapper>
+            cards.push(<CardWrapper key={this.props.headlines[0].id}>
                 <HeadlineCard icon = {icons[this.rand(icons.length - 1)]} key = {this.props.headlines[0].id} item = {this.props.headlines[0]}/>
             </CardWrapper>);
         } else {
             for (let i = 0; i <= this.props.headlines.length - 3; i += 3){
-                cards.push(<CardWrapper>
+                cards.push(<CardWrapper key={this.props.headlines[i].id}>
                     <HeadlineCard icon = {icons[this.rand(icons.length - 1)]} key = {this.props.headlines[i].id} item = {this.props.headlines[i]}/>
                     <HeadlineCard icon = {icons[this.rand(icons.length - 1)]} key = {this.props.headlines[i + 1].id} item = {this.props.headlines[i + 1]}/>
                     <HeadlineCard icon = {icons[this.rand(icons.length - 1)]} key = {this.props.headlines[i + 2].id} item = {this.props.headlines[i + 2]}/>
